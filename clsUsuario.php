b5625cc8d080f217d3602c47545b8b63b52a31a5
Soraya del Real <soraya.delreal.carrasco@gmail.com>
2025-03-27 21:34:42 +0100

    Crear getters y setters del usuario

--- clsUsuario.php
+++ clsUsuario.php
@@ -3,29 +3,65 @@
 require_once 'conexion.php';
 
 class Usuario extends Conexion {
-    public $id;
-    public $nombre;
-    public $email;
-    public $pass;
-    public $codigo;
-    public $horas;
-    public $rol;
+    private $id;
+    private $nombre;
+    private $email;
+    private $pass;
+    private $codigo;
+    private $horas;
+    private $rol;
 
     public function login() {
-        try {
-            $sql = "SELECT email, password FROM usuarios WHERE email = :email AND password = :password";
-            $bd_conexion = $this->conecta()->prepare($sql);
-            $bd_conexion->bindParam(':email', $this->email);
-            $bd_conexion->bindParam(':password', $this->pass);
-            $bd_conexion->execute();
-            $respuesta = $bd_conexion->fetch();
-            
-            if(!$respuesta) throw new Exception("El usuario no es correcto o no existe");
+        $sql = "SELECT * FROM usuarios WHERE email = :email AND password = :password";
+        $bd_conexion = $this->conecta()->prepare($sql);
+        $bd_conexion->bindParam(':email', $this->email);
+        $bd_conexion->bindParam(':password', $this->pass);
+        $bd_conexion->execute();
+        $respuesta = $bd_conexion->fetch();
+        
+        if(!$respuesta) return false;
 
-            return true;
-        } catch (Exception $e) {
-            throw new Exception($e->getMessage());
-        }
+        $this->id = $respuesta["id"];
+        $this->nombre = $respuesta["Nombre"];
+        $this->email = $respuesta["Email"];
+        $this->pass = null;
+        $this->codigo = $respuesta["Codigo"];
+        $this->horas = $respuesta["Horas"];
+        $this->rol = $respuesta["Rol"];
+
+        return true;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getCodigo() {
+        return $this->codigo;
+    }
+
+    public function getHoras() {
+        return $this->horas;
+    }
+
+    public function getRol() {
+        return $this->rol;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setPass($password) {
+        $this->pass = $password;
     }
 }
 
