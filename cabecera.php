1597a0a874233a3aa36a99c4f1bd0cc63d03cfa1
Soraya del Real <soraya.delreal.carrasco@gmail.com>
2025-03-28 18:55:39 +0100

    Maquetar cabecera

--- cabecera.php
+++ cabecera.php
@@ -10,21 +10,30 @@
     <h1>Salle Presencia</h1>
   </div>
 
-  <?php if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] === "admin"): ?>
-    <ul>
-      <li>Listado usuarios</li>
-      <li>Listado fichadas</li>
-    </ul>
-  <?php elseif (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] === "user"): ?>
-    <ul>
-      <li>Mis fichadas</li>
-    </ul>
-  <?php endif; ?>
+  <div class="row">
+    <div class="col-6">
+      <?php if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] === "admin"): ?>
+        <nav class="nav">
+          <a class="nav-link" href="gestionusuarios.php">Listado usuarios</a>
+          <a class="nav-link" href="gestionfichadas.php">Gestion fichadas</a>
+        </nav>
+      <?php elseif (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] === "user"): ?>
+        <nav class="nav">
+          <a class="nav-link" href="fichar.php">Mis fichadas</a>
+        </nav>
+      <?php endif; ?>
+    </div>
+    <div class="col-md-6 d-flex justify-content-end align-items-center">
+      <?php if (isset($_SESSION["user"])): ?>
+        <button class="btn btn-outline-success me-2">
+          Información usuario: <?php echo $_SESSION["user"]["nombre"] ?>
+        </button>
+        <a href="logout.php" class="btn btn-outline-danger">
+          Logout
+        </a>
+      <?php endif; ?>
+    </div>
+  </div>
 
-  <?php if (isset($_SESSION["user"])): ?>
-    Información usuario: info@test.com
-    <a href="logout.php" class="btn btn-primary">
-      Logout
-    </a>
-  <?php endif; ?>
+
 </header>
--- fichar.php
+++ fichar.php
@@ -6,10 +6,16 @@
 </head>
 
 <body>
-  <?php
-  require_once "cabecera.php";
-  ?>
-  Hola usuario
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <?php
+        require_once "cabecera.php";
+        ?>
+        Hola usuario
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
