2ca6685d552374c196907e3a2655e27fd7895057
Soraya del Real <soraya.delreal.carrasco@gmail.com>
2025-03-31 20:48:01 +0200

    Añadir constantes a la conexion

--- conexion.php
+++ conexion.php
@@ -1,12 +1,21 @@
 <?php	
     class Conexion {
-        
-        public function conecta() {
+        const HOST = "localhost";
+        const BASE_DE_DATOS = "sallepresencia";
+        const USUARIO = "root";
+        const CONTRASENA = "";
+
+        public function conecta(
+            $host = self::HOST,
+            $base_de_datos = self::BASE_DE_DATOS,
+            $usuario = self::USUARIO,
+            $contrasena = self::CONTRASENA,
+        ) {
             try {
                 $link = new PDO(
-                    "mysql:host=localhost;dbname=sallepresencia",
-                    "root",
-                    "",
+                    "mysql:host=".$host.";dbname=".$base_de_datos,
+                    $usuario,
+                    $contrasena,
                 );
                 return $link;
             } catch (PDOException $e) {
